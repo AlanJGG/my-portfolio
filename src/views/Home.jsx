d94831d8d18ca10c2ac20849481b9dc57bf12d20
AlanJGG <alanguzmangutierrez2005@gmail.com>
2024-10-11 11:59:55 -0400

    Set deploy settings

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,11 +2,18 @@ export const Home = () => {
   return (
     <div id="home" className="p-28 2xl:pb-48 md:flex justify-center 2xl:mt-28">
       <div className="my-10 md:my-28  ml-5">
-        <h2 className="pl-6 text-xl md:text-2xl lg:text-2xl xl:text-3xl antialiased">Hi, I'm Alan.</h2>
-        <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold antialiased">I give fast and innovative solutions.</h1>
+        <h2 className="pl-6 text-xl md:text-2xl lg:text-2xl xl:text-3xl antialiased">
+          Hi, I'm Alan.
+        </h2>
+        <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold antialiased">
+          I give fast and innovative solutions.
+        </h1>
       </div>
       <div className="md:pl-40 md:pt-2">
-        <img className="brightness-75 grayscale-0 lg:size-96 xl:size-auto rounded-xl md:max-w-[400px] sm:size-auto" src="/assets/draft-face.png"/>
+        <img
+          className="brightness-75 grayscale-0 lg:size-96 xl:size-auto rounded-xl md:max-w-[400px] sm:size-auto"
+          src={`${import.meta.env.BASE_URL}assets/draft-face.png`}
+        />
       </div>
     </div>
   );
--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/my-portfolio/" : "/",
   server: {
     hmr: {
       overlay: true, // Esto muestra los errores en el navegador
     },
   },
   plugins: [react()],
-});
+}));
