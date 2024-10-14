c108b446c2018224a26885ef78a3512d3d569e5d
AlanJGG <alanguzmangutierrez2005@gmail.com>
2024-10-13 21:34:45 -0400

    Section shadow

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,7 +1,7 @@
 export const About = () => {
   return (
     <div id="about" className="xl:h-screen p-4 lg:p-28 antialised 2xl:pb-40">
-      <div className="h-full bg-slate-800 bg-opacity-25 p-8 2xl:p-16 rounded-3xl">
+      <div className="h-full shadow-md ease-in-out duration-300 hover:shadow-lg bg-slate-800 bg-opacity-25 p-8 2xl:p-16 rounded-3xl">
         <h1 className="sm:flex 2xl:ml-48 text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold antialiased ">
           About me
         </h1>
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -5,7 +5,7 @@ import { LinkButton } from "../components";
 export const Contact = () => {
   return (
     <div id="contact" className="p-4 lg:p-28 antialised">
-      <div className="h-full bg-slate-800 bg-opacity-25 p-8 2xl:p-16 rounded-3xl">
+      <div className="h-full shadow-md ease-in-out duration-300 hover:shadow-lg  bg-slate-800 bg-opacity-25 p-8 2xl:p-16 rounded-3xl">
         <h1 className="sm:flex 2xl:ml-48 text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold antialiased ">
           Contact me
         </h1>
